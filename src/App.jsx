9c921f1d3a991b06ade7395a196770d9b94528a4
Utkarsh Patidar <patidarutkarsh183@gmail.com>
2026-03-21 19:13:21 +0530

    Basic Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+let app = () => {
+    return (
+        <div className="h-screen w-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-gray-800">Git Inspector</h1>
+        </div>
+    );
+}
+
+export default app;
